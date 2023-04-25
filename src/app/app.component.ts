87f1369923471d43615cac8bb49ce38a7bbd6eba
Arthur Sampaio Teodosio <124711045+ArthurTeodosio@users.noreply.github.com>
2023-04-25 20:08:36 -0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ export class AppComponent {
  arrayDeTarefas: Tarefa[] = [];
  apiURL : string;
  constructor(private http: HttpClient) {
- this.apiURL = 'todoapp-backend-ac.vercel.app';
+ this.apiURL = 'https://todoapp-backend-git-master-arthurteodosio.vercel.app';
  this.READ_tarefas();
  }
  CREATE_tarefa(descricaoNovaTarefa: string) {
